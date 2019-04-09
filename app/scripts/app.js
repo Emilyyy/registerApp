a0b30ed8e203228bd01e808335e8babb7381ee0b
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-04-09 16:14:41 -0400

    environment setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name registerAppApp
+ * @description
+ * # registerAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('registerAppApp', []);
