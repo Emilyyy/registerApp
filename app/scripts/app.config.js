45d7c128b0424d56cf5e8dd5692b1027704345ff
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-04-09 16:45:27 -0400

    register view

---
+++ app/scripts/app.config.js
@@ -0,0 +1,17 @@
+angular.module('registerAppApp').config(function($urlRouterProvider, $stateProvider){
+
+    $urlRouterProvider.otherwise('/');
+    
+    $stateProvider
+
+    .state('main',{
+        url: '/',
+        templateUrl: '/views/main.html'
+    })
+
+    .state('register',{
+        url: '/register',
+        templateUrl: '/views/register.html'
+    });
+
+})
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,7 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name registerAppApp
- * @description
- * # registerAppApp
- *
- * Main module of the application.
- */
 angular
-  .module('registerAppApp', []);
+  .module('registerAppApp', ['ui.router'])
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('');
+  }]);
