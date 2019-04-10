9ddef04481c16d8a4e102487dabff38357ca8220
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-04-10 01:58:37 -0400

    API Register Post

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -3,8 +3,10 @@ angular.module('registerAppApp')
   .controller('RegisterCtrl', function($scope, $rootScope, $http, alert) {
     $scope.submit = function(){  
 
-      var url = '/';
-      var user = {};
+      var url = 'http://localhost:3000/register';
+      var user = {
+        name:'Alex'
+      };
       $http.post(url,user).then(function(res){
         alert('success', 'OK!', 'You are now registered');
       }, function(err){
