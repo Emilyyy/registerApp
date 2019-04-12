4275800d15e1cf22b97e89997cfd68c94a6f7f30
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-04-12 02:23:20 -0400

    MongoDB Save User

--- frontend/app/scripts/controllers/register.js
+++ frontend/app/scripts/controllers/register.js
@@ -5,7 +5,8 @@ angular.module('registerAppApp')
 
       var url = 'http://localhost:3000/register';
       var user = {
-        name:'Alex'
+        email: $scope.email,
+        password: $scope.password
       };
       $http.post(url,user).then(function(res){
         alert('success', 'OK!', 'You are now registered');
