68efc730ca0980b5cca0377084a67b405c3ebe47
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-04-16 00:43:06 -0400

    authToken bug fix

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -4,4 +4,23 @@ angular
   .module('registerAppApp', ['ui.router'])
   .config(['$locationProvider', function($locationProvider) {
     $locationProvider.hashPrefix('');
-  }]);
+  }])
+  .factory('authToken', function($window){
+    var storage = $window.localStorage;
+    var cachedToken;
+    return{
+        setToken: function(token){
+            cachedToken = token;
+            storage.setItem('userToken', token);
+        },
+        getToken: function(){
+            if(!cachedToken)
+                cachedToken = storage.getItem('userToken');
+
+                return cachedToken;
+        },
+        isAuthenticated: function(){
+            return !!this.getToken();
+        }
+    };
+});;
--- frontend/app/scripts/services/authtoken.js
+++ frontend/app/scripts/services/authtoken.js
@@ -1,22 +1,3 @@
 'use strict';
 
-angular.module('registerAppApp')
-    .factory('authToken', function($window){
-        var storage = $window.localStorage;
-        var cachedToken;
-        return{
-            setToken: function(token){
-                cachedToken = token;
-                storage.setItem('userToken', token);
-            },
-            getToken: function(){
-                if(!cachedToken)
-                    cachedToken = storage.getItem('userToken');
-
-                    return cachedToken;
-            },
-            isAuthenticated: function(){
-                return !!this.getToken();
-            }
-        };
-    });
+// angular.module('registerAppApp')
