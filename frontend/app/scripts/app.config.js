1af00799278e3b3949258393aecd620f1707eab9
Emilyyy <emilyliu@Emilys-MacBook-Pro.local>
2019-05-02 01:19:43 -0400

    Login View

--- frontend/app/scripts/app.config.js
+++ frontend/app/scripts/app.config.js
@@ -16,6 +16,12 @@ angular.module('registerAppApp').config(function($urlRouterProvider, $stateProvi
         templateUrl: '/views/register.html',
         controller: 'RegisterCtrl'
     })
+
+    .state('login',{
+        url: '/login',
+        templateUrl: '/views/login.html',
+        controller: 'LoginCtrl'
+    })
     
     .state('logout',{
         url: '/logout',
---
+++ frontend/app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name registerAppApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the registerAppApp
+ */
+angular.module('registerAppApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
